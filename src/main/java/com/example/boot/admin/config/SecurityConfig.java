13290fc13f9f3ac67490eb688dcd6505afd47a6c
prabhug1987 <prabhug1987@gmail.com>
2021-09-04 19:27:12 +0530

    UI changes & configured mail

--- src/main/java/com/example/boot/admin/config/SecurityConfig.java
+++ src/main/java/com/example/boot/admin/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.boot.admin.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -9,22 +10,23 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import ch.qos.logback.classic.Logger;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
 		//super.configure(http);
 		SavedRequestAwareAuthenticationSuccessHandler authenticationSuccessHandler = new SavedRequestAwareAuthenticationSuccessHandler();
 		authenticationSuccessHandler.setTargetUrlParameter("redirectTo");
 		authenticationSuccessHandler.setDefaultTargetUrl("/");
 		http.authorizeRequests().antMatchers("/assets/**").permitAll()
-		                        .antMatchers("login").permitAll()
+		                        .antMatchers("/login").permitAll()
 		                        .anyRequest().authenticated()
 		                        .and().formLogin().loginPage("/login")
 		                        .successHandler(authenticationSuccessHandler)
-		                        .and().logout().logoutUrl("logout")
+		                        .and().logout().logoutUrl("/logout")
 		                        .and().httpBasic()
 		                        .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-		                        .ignoringAntMatchers("/instances","/actuator/**");
+		                        .ignoringAntMatchers("/instances");
+		//super.configure(http);
 	}
 }
